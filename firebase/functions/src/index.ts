c3eb65caea16ef113d63d1e829064570b5aaa7d2
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-11-08 08:48:14 +0900

    feat: add createPaymentIntent functions
    feat: 新機能
    fix: バグ修正・レビュー反映
    docs: ドキュメントの変更
    style: コードのロジックに影響を与えない変更（ホワイトスペース、フォーマット、セミコロンの欠落など）
    refactor: バグ修正でも機能追加でもないコード変更
    perf: パフォーマンス向上
    test: テストの追加や既存のテストの修正
    chore: ビルドプロセス、またはドキュメント生成などの補助ツールやライブラリの変更

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,31 @@
 import * as functions from "firebase-functions";
 
-export const helloWorld = functions
+const stripe = require("stripe")(process.env.STRIPE_SK_TEST);
+
+export const createPaymentIntent = functions
   .region("asia-northeast1")
-  .https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
+  .https.onRequest(async (req, res) => {
+    // customer（Stripe上のユーザーアカウント）の作成
+    const customer = await stripe.customers.create();
+    // 一時的なキーを作成
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: customer.id },
+      { apiVersion: "2022-08-01" }
+    );
+    // 決済フォームを作成
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1000,
+      currency: "jpy",
+      customer: customer.id,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    // 返却
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      publishableKey: process.env.STRIPE_PK_TEST,
+    });
   });
