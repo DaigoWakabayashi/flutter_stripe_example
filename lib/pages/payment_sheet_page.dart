84cefad415eb597390529800b8528a5ce60442a3
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-11-19 15:50:48 +0900

    feat: show PaymentSheet
    feat: 新機能
    fix: バグ修正・レビュー反映
    docs: ドキュメントの変更
    style: コードのロジックに影響を与えない変更（ホワイトスペース、フォーマット、セミコロンの欠落など）
    refactor: バグ修正でも機能追加でもないコード変更
    perf: パフォーマンス向上
    test: テストの追加や既存のテストの修正
    chore: ビルドプロセス、またはドキュメント生成などの補助ツールやライブラリの変更

--- lib/pages/payment_sheet_page.dart
+++ lib/pages/payment_sheet_page.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 class PaymentSheetPage extends StatelessWidget {
   const PaymentSheetPage({super.key});
@@ -7,7 +11,45 @@ class PaymentSheetPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(runtimeType.toString())),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  // 1. Cloud Functions 上で PaymentIntent を作成
+                  final callable = FirebaseFunctions.instance
+                      .httpsCallable('createPaymentIntent');
+                  final result = await callable.call();
+                  final data = result.data;
+                  log(result.data.toString());
+
+                  // 2. PaymentSheet を初期化
+                  await Stripe.instance.initPaymentSheet(
+                    paymentSheetParameters: SetupPaymentSheetParameters(
+                      customFlow: true,
+                      merchantDisplayName: 'Flutter Stripe Example',
+                      paymentIntentClientSecret: data['paymentIntent'],
+                      customerEphemeralKeySecret: data['ephemeralKey'],
+                      customerId: data['customer'],
+                    ),
+                  );
+
+                  // 3. PaymentSheet を表示
+                  await Stripe.instance.presentPaymentSheet();
+
+                  // 4. 決済を確定
+                  await Stripe.instance.confirmPaymentSheetPayment();
+                } catch (e) {
+                  log(e.toString());
+                }
+              },
+              child: const Text('Call PaymentIntent API'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
