64ed8344dbeac9060337f060dec5cd5b72581d64
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-12-01 00:59:29 +0900

    feat: retrieve payment info
    feat: 新機能
    fix: バグ修正・レビュー反映
    docs: ドキュメントの変更
    style: コードのロジックに影響を与えない変更（ホワイトスペース、フォーマット、セミコロンの欠落など）
    refactor: バグ修正でも機能追加でもないコード変更
    perf: パフォーマンス向上
    test: テストの追加や既存のテストの修正
    chore: ビルドプロセス、またはドキュメント生成などの補助ツールやライブラリの変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'dart:developer';
+
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:flutter_stripe_example/config/app_color.dart';
-import 'package:flutter_stripe_example/pages/card_field_page.dart';
-import 'package:flutter_stripe_example/pages/card_form_page.dart';
-import 'package:flutter_stripe_example/pages/payment_sheet_page.dart';
 
 import 'firebase_options.dart';
 
@@ -26,48 +27,74 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: AppColor.primary),
-      home: const BasePage(),
+      home: const PaymentSheetPage(),
     );
   }
 }
 
-class BasePage extends StatelessWidget {
-  const BasePage({super.key});
+class PaymentSheetPage extends HookWidget {
+  const PaymentSheetPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final payment = useState<PaymentIntent?>(null);
     return Scaffold(
+      appBar: AppBar(title: Text(runtimeType.toString())),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text('金額：${payment.value?.amount.toString()}'),
+            Text('ステータス：${payment.value?.status.name.toString()}'),
+            Text('日時：${payment.value?.created.toString()}'),
             ElevatedButton(
-              onPressed: () => Navigator.push<void>(
-                context,
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) => const PaymentSheetPage(),
-                ),
-              ),
-              child: const Text('PaymentSheet'),
+              onPressed: () async {
+                try {
+                  // 1. Cloud Functions 上で PaymentIntent を作成
+                  final callable = FirebaseFunctions.instance
+                      .httpsCallable('createPaymentIntent');
+                  final result = await callable.call();
+                  final data = result.data;
+
+                  // 2. PaymentSheet を初期化
+                  await Stripe.instance.initPaymentSheet(
+                    paymentSheetParameters: SetupPaymentSheetParameters(
+                      customFlow: true,
+                      merchantDisplayName: 'Flutter Stripe Example',
+                      paymentIntentClientSecret: data['paymentIntent'],
+                      customerEphemeralKeySecret: data['ephemeralKey'],
+                      customerId: data['customer'],
+                    ),
+                  );
+
+                  // 3. PaymentSheet を表示
+                  await Stripe.instance.presentPaymentSheet();
+
+                  // 4. 決済を確定
+                  await Stripe.instance.confirmPaymentSheetPayment();
+
+                  // 5. 決済内容を取得・表示
+                  final paymentIntent = await Stripe.instance
+                      .retrievePaymentIntent(data['paymentIntent']);
+                  payment.value = paymentIntent;
+                } on StripeException catch (e) {
+                  final error = e.error;
+                  switch (error.code) {
+                    case FailureCode.Canceled:
+                      log('キャンセルされました', error: e);
+                      break;
+                    case FailureCode.Failed:
+                      log('エラーが発生しました', error: e);
+                      break;
+                  }
+                } on FirebaseFunctionsException catch (e) {
+                  log('エラーが発生しました', error: e);
+                } catch (e) {
+                  log('不明なエラーが発生しました', error: e);
+                }
+              },
+              child: const Text('Show PaymentSheet'),
             ),
-            ElevatedButton(
-              onPressed: () => Navigator.push<void>(
-                context,
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) => const CardFieldPage(),
-                ),
-              ),
-              child: const Text('CardField'),
-            ),
-            ElevatedButton(
-              onPressed: () => Navigator.push<void>(
-                context,
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) => const CardFormPage(),
-                ),
-              ),
-              child: const Text('CardForm'),
-            )
           ],
         ),
       ),
--- lib/pages/payment_sheet_page.dart
+++ lib/pages/payment_sheet_page.dart
@@ -1,67 +1,10 @@
-import 'dart:developer';
-
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
 
 class PaymentSheetPage extends StatelessWidget {
   const PaymentSheetPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(runtimeType.toString())),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  // 1. Cloud Functions 上で PaymentIntent を作成
-                  final callable = FirebaseFunctions.instance
-                      .httpsCallable('createPaymentIntent');
-                  final result = await callable.call();
-                  final data = result.data;
-                  log(result.data.toString());
-
-                  // 2. PaymentSheet を初期化
-                  await Stripe.instance.initPaymentSheet(
-                    paymentSheetParameters: SetupPaymentSheetParameters(
-                      customFlow: true,
-                      merchantDisplayName: 'Flutter Stripe Example',
-                      paymentIntentClientSecret: data['paymentIntent'],
-                      customerEphemeralKeySecret: data['ephemeralKey'],
-                      customerId: data['customer'],
-                    ),
-                  );
-
-                  // 3. PaymentSheet を表示
-                  await Stripe.instance.presentPaymentSheet();
-
-                  // 4. 決済を確定
-                  await Stripe.instance.confirmPaymentSheetPayment();
-                } on StripeException catch (e) {
-                  final error = e.error;
-                  switch (error.code) {
-                    case FailureCode.Canceled:
-                      log('キャンセルされました', error: e);
-                      break;
-                    case FailureCode.Failed:
-                      log('エラーが発生しました', error: e);
-                      break;
-                  }
-                } on FirebaseFunctionsException catch (e) {
-                  log('エラーが発生しました', error: e);
-                } catch (e) {
-                  log('不明なエラーが発生しました', error: e);
-                }
-              },
-              child: const Text('Call PaymentIntent API'),
-            )
-          ],
-        ),
-      ),
-    );
+    return const SizedBox();
   }
 }
